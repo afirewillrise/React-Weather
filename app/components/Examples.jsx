54da04c74ca60b7e3c2cdb2cc3e1599768f8d217
afirewillrise <monishkumar1154@gmail.com>
2016-08-17 15:17:59 -0400

    Testing Small Change

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -12,7 +12,7 @@ var Examples = (props) => {
   return (
     <div>
       <h3>Examples</h3>
-      <p>Welcome to Examples page!</p>
+      <p>Welcome to Examples page and hell yeah! This baby is live!</p>
     </div>
   );
 };
